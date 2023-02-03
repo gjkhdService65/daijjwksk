8021155f92b1352b6515adf75bd9638332c42fe9
gjkhdService65 <124329420+gjkhdService65@users.noreply.github.com>
2023-02-03 06:03:30 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=4e83af19-8038-49f2-9280-650f5aea1360
 
 #请修改为自己设置的伪装站，不要带https://
 export ProxySite=www.holehike.com
